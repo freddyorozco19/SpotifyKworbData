cb8dac7ea0df89645610ec91063afaba20e848df
Freddy Orozco <60451879+freddyorozco19@users.noreply.github.com>
2025-02-09 09:01:35 -0500

    Update KworbExtractData.py

--- KworbExtractData.py
+++ KworbExtractData.py
@@ -51,6 +51,6 @@ df = pd.DataFrame({
 })
 
 df['Streams totales'] = df['Streams totales'].astype(int)
-df['Streams diarios'] = df['Streams diarios'].astype(int)
+#df['Streams diarios'] = df['Streams diarios'].astype(int)
 
 st.dataframe(df)
