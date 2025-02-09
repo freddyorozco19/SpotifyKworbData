019205b9a841c02ccdbb851e1796f32c5b0f68c8
Freddy Orozco <60451879+freddyorozco19@users.noreply.github.com>
2025-02-09 09:05:30 -0500

    Update KworbExtractData.py

--- KworbExtractData.py
+++ KworbExtractData.py
@@ -52,7 +52,7 @@ df = pd.DataFrame({
 })
 
 df['Streams totales'] = df['Streams totales'].astype(int)
-df['Streams diarios'] = df['Streams diarios'].replace(np.nan, 0)
+df['Streams diarios'] = df['Streams diarios'].replace('', 0)
 
 st.write(df['Streams diarios'])
 df['Streams diarios'] = df['Streams diarios'].astype(int)
