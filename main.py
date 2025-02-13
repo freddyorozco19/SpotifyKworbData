84b8646893cc33b9fc95e1804bde2948a04c1f78
Freddy Orozco <60451879+freddyorozco19@users.noreply.github.com>
2025-02-13 00:52:13 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,57 +1,41 @@
 import streamlit as st
-from bs4 import BeautifulSoup
-import requests
+#import hydralit_components as hc
+import datetime
+import base64
+import pandas as pd
+from io import BytesIO
 import pandas as pd
 import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib.pyplot as mplt
+import matplotlib.font_manager as font_manager
+import mplsoccer
+from mplsoccer import Pitch, VerticalPitch, FontManager
+import sklearn
+from sklearn.preprocessing import StandardScaler
+from scipy.spatial import ConvexHull
+from matplotlib.colors import LinearSegmentedColormap
+import matplotlib.patheffects as path_effects
+from scipy.ndimage import gaussian_filter
+import seaborn as sns
+from matplotlib import colors as mcolors
+import requests
+#from PIL import Image
+from matplotlib.patches import Rectangle
+import math
+
+############################################################################################################################################################################################################################
 
 st.set_page_config(layout="wide")
-st.header("KWORB EXTRACT DATA")
-st.divider()
 
-url = st.text_input("URL")
+navigation_tree = {
+    "Menu": [
+        st.Page("cont/KworbExtractData.py", title="Artist Data", icon=":material/download:"),
+        #st.Page("cont/FinData.py", title="Fin Data", icon=":material/cell_merge:"),
+        #st.Page("cont/FitStats.py", title="Fit Stats", icon=":material/analytics:"),
+        st.Page("cont/KworbCompareData.py", title="Register Data", icon=":material/leaderboard:")]
+}
 
-# Obtener el contenido de la página
-respuesta = requests.get(url)
-html = respuesta.text
-# Crear el objeto BeautifulSoup
-soup = BeautifulSoup(html, 'html.parser', from_encoding='utf-8')
-# Listas para almacenar los datos
-canciones = []
-urls = []
-streams_totales = []
-streams_diarios = []
-# Encontrar todas las filas (tr) de la tabla
-filas = soup.find_all('tr')
-# Recorrer cada fila y extraer la información
-for fila in filas:
-    # Encontrar todas las celdas (td) en la fila
-    celdas = fila.find_all('td')
-    
-    # Si la fila tiene celdas, procesarla
-    if celdas:
-        # Para el título de la canción (está dentro de un enlace)
-        titulo = celdas[0].find('a')
-        if titulo:
-            canciones.append(titulo.text)
-            urls.append(titulo['href'])
-            streams_totales.append(celdas[1].text.replace(",", ""))
-            streams_diarios.append(celdas[2].text.replace(",", ""))
-            
+nav = st.navigation(navigation_tree, position="sidebar")
 
-
-# Crear el DataFrame
-df = pd.DataFrame({
-    'Canción': canciones,
-    'URL': urls,
-    'Streams totales': streams_totales,
-    'Streams diarios': streams_diarios
-})
-
-df['Streams totales'] = df['Streams totales'].astype(int)
-df['Streams diarios'] = df['Streams diarios'].replace('', 0)
-df['Streams diarios'] = df['Streams diarios'].astype(int)
-
-
-st.dataframe(df)
-st.metric("Date", 
-st.divider()
+nav.run()
